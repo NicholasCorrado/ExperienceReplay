8be0eb00a949d58e6e4387153bb1eb495bdf5758
ncorrado <ncorrado@ap2001.chtc.wisc.edu>
2025-04-04 12:33:35 -0500

    Update chtc scripts

--- chtc/job.sh
+++ chtc/job.sh
@@ -7,18 +7,24 @@ cmd="${cmd} --run_id ${step} --seed ${step}"
 echo $cmd
 
 # fetch your code from /staging/
-CODENAME=PROPS
+CODENAME=ExperienceReplay
 cp /staging/ncorrado/${CODENAME}.tar.gz .
 tar -xzf ${CODENAME}.tar.gz
 rm ${CODENAME}.tar.gz
 
-cd PROPS
-export PYTHONPATH=custom-envs:$PYTHONPATH # pip install -e fails on chtc because we don't have admin privileges .
-mkdir gymnasium_local
-python -m pip install 'gymnasium[mujoco]==0.29.1' --target gymnasium_local
-python -m pip install 'mujoco==2.3.5' --target mujoco_local
-export PYTHONPATH=gymnasium_local:$PYTHONPATH
-export PYTHONPATH=mujoco_local:$PYTHONPATH
+cd ${CODENAME}
+mkdir local
+python -m pip install -r requirements.txt --target local
+python -m pip install 'gymnasium[mujoco]==0.29.1' --target local
+export PYTHONPATH=local:$PYTHONPATH
+
+#export PYTHONPATH=custom-envs:$PYTHONPATH # pip install -e fails on chtc because we don't have admin privileges .
+#mkdir gymnasium_local
+#python -m pip install 'gymnasium[mujoco]==0.29.1' --target gymnasium_local
+#python -m pip install 'mujoco==2.3.5' --target mujoco_local
+#export PYTHONPATH=gymnasium_local:$PYTHONPATH
+#export PYTHONPATH=mujoco_local:$PYTHONPATH
+
 $cmd
 
 # compress results. This file will be transferred to your submit node upon job completion.
